1336ff5af50084fff0c7926f6606088fd4839af5
Ashwinajackus <ashwin.rajaram@ajackus.com>
2025-03-08 16:03:54 +0530

    doctor api

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ require('./config/mongoose');
 
 const PORT = 3000;
 const loginRoute = require('./routes/login')
+const doctorRoute = require('./routes/doctor')
 app.use(cors()) // Cross-Origin Resource Sharing
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.use('/user',loginRoute);
+app.use('/doctor', doctorRoute)
 
 app.listen(PORT, ()=>{
     console.log("Server is running on the port", PORT)
